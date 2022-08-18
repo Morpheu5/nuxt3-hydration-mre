437d1e8f5fa4ad74bb769095fbe157e440e73af6
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-08-18 09:11:12 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>{{ page_title }}</h1>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+    const page_title = useState('page_title');
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Some page content.</div>
+</template>
+
+<script setup>
+    useState('page_title', () => 'Some page title');
+</script>
